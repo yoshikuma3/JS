56e487523c0847834767eb905b2ef47939aa1d4e
yoshikuma3 <yoshikuma.0801@gmail.com>
2023-03-10 11:42:26 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 window.addEventListener('load', function(){
 
     const pullDownButton = document.getElementById("lists")
-    console.log(pullDownButton)
+    const pullDownParents = document.getElementById("pull-down")
 
   pullDownButton.addEventListener('mouseover', function(){
     const pullDownButton = document.getElementById("lists")
-  })
+
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
+    this.setAttribute("style", "background-color:blue;")
   })
+})
 
   pullDownButton.addEventListener('click', function(){
     const pullDownButton = document.getElementById("lists")
-  })
+
   pullDownButton.addEventListener('click', function(){
-    pullDownButton.setAttribute("style", "background-color:green;")
+    this.setAttribute("style", "display:block;")
+  pullDownButton.addEventListener('click', function(){
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+      pullDownParents.removeAttribute("style", "display:block;")
+      console.log("非表示")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+      console.log("表示")
+    }
   })
+})
+})
 
   pullDownButton.addEventListener('mouseout', function(){
     const pullDownButton = document.getElementById("lists")
-  })
+
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "backgound-color:red;")
+    this.removeAttribute("style", "backgound-color:red;")
   })
+})
 })
