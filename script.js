313203dbf5081422395335fda64f902ce71c571e
yoshikuma3 <yoshikuma.0801@gmail.com>
2023-03-22 12:24:27 +0900

    文字列の取得編

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+function pullDown() {
+}
+window.addEventListener('load',pullDown)
+
 window.addEventListener('load', function(){
 
     const pullDownButton = document.getElementById("lists")
     const pullDownParents = document.getElementById("pull-down")
+    const pullDoenChild = document.querySelectorAll(".pull-down-list")
+    const currentList = document.getElementById("current-list")
+
+  pullDoenChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      const value = list.innerHTML
+      currentList.innerHTML = value
+      console.log(value)
+    })
+  })
 
   pullDownButton.addEventListener('mouseover', function(){
     const pullDownButton = document.getElementById("lists")
@@ -12,11 +26,6 @@ window.addEventListener('load', function(){
 })
 
   pullDownButton.addEventListener('click', function(){
-    const pullDownButton = document.getElementById("lists")
-
-  pullDownButton.addEventListener('click', function(){
-    this.setAttribute("style", "display:block;")
-  pullDownButton.addEventListener('click', function(){
     if (pullDownParents.getAttribute("style") == "display:block;"){
       pullDownParents.removeAttribute("style", "display:block;")
       console.log("非表示")
@@ -24,8 +33,6 @@ window.addEventListener('load', function(){
       pullDownParents.setAttribute("style", "display:block;")
       console.log("表示")
     }
-  })
-})
 })
 
   pullDownButton.addEventListener('mouseout', function(){
